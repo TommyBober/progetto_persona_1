bf9e05eb564933016f4dba0ed99d77697ac7a33d
TommyBober <tommyru2000@gmail.com>
2017-01-19 21:19:41 +0100

    primo commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/persona.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class persona
+    {
+        private string _nome;
+        public string nome { get; set; }
+
+        private string _cognome;
+        public string cognome { get; set; }
+
+        private int _anni;
+        public int anni
+        {
+            set
+            {
+                if (value <= 0) value = 1;
+                _anni = value;
+            }
+
+            get { return _anni; }
+        }
+
+        private string _codicefiscale;
+        public string codicefiscale
+        {
+            get { return nome + cognome + anni; }
+        }
+
+        public persona(string nome, string cognome, int anni)
+        {
+            this.anni = anni;
+            this.nome = nome;
+            this.cognome = cognome;
+
+        }
+
+        public virtual void describe()
+        {
+            Console.WriteLine("Questo è " + nome + "  " + cognome);
+            Console.WriteLine("di anni :" + anni);
+            Console.WriteLine("codice fiscale:" + codicefiscale);
+        }
+
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/studente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class studente : persona
+
+    {
+        private string _sezione;
+        public string sezione { get; set; }
+
+        public studente(string nome, string cognome, int anni, string sezione) : base(nome, cognome, anni)
+        {
+            this.sezione = sezione;
+        }
+        public override void describe()
+        {
+            base.describe();
+            Console.WriteLine("sezione" + sezione + System.Environment.NewLine);
+        }
+    }
+}
